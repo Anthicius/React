90d91c4e2b03a584894951e4c22106dfd4a5bd17
Anthicius <andrius.makseckas@gmail.com>
2024-12-15 18:04:56 +0200

    new changes

--- 10-reacting-to-events/src/App.jsx
+++ 10-reacting-to-events/src/App.jsx
@@ -1,11 +1,14 @@
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
 function App() {
-  function handleSelect() {
-    console.log('Hello World - selected!');
+  const [selectedTopic, setSelectedTopic] = useState(null);
+
+  function handleSelect(selectedOption) {
+    setSelectedTopic(selectedOption);
   }
 
   return (
@@ -28,12 +31,25 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+          <div id="tab-content">
+            {!selectedTopic ? (<p>Please select a topic!</p>):
+            (
+              <div>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+            </div>
+            )}
+          </div>
         </section>
       </main>
     </div>
